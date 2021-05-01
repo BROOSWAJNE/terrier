749a896d27e4eaac3323003335609bcbbd0919af
Willem Garnier <willem.garnier@gmail.com>
2021-05-01 18:22:30 +0100

    initial module

---
+++ lib/colors.ts
@@ -0,0 +1,63 @@
+import { inspect } from 'util';
+
+// standard colour codes
+
+export const CODE_RESET_ALL = 0;
+export const CODE_RESET_INTENSITY = 22;
+export const CODE_RESET_ITALIC = 23;
+export const CODE_RESET_UNDERLINE = 24;
+export const CODE_RESET_COLOR = 39;
+
+export const CODE_BOLD = 1;
+export const CODE_DIM = 2;
+export const CODE_ITALIC = 3;
+export const CODE_UNDERLINE = 4;
+
+export const CODE_BLACK = 30;
+export const CODE_RED = 31;
+export const CODE_GREEN = 32;
+export const CODE_YELLOW = 33;
+export const CODE_BLUE = 34;
+export const CODE_MAGENTA = 35;
+export const CODE_CYAN = 36;
+export const CODE_WHITE = 37;
+
+/** Whether colours are enabled for the current session. */
+export const COLORS_ENABLED = process.env.NO_COLOR == null
+	&& process.env.TERM !== 'dumb'
+	&& process.stdout.isTTY;
+export const ESCAPE = '\x1b[';
+
+/** A function which colorises the given arguments. */
+export type Color = (...args: unknown[]) => string;
+
+const stringify = (thing: unknown) => typeof thing === 'string'
+	? thing
+	: inspect(thing, { colors: false });
+/** Creates a colorizer function for the given color code. */
+export const createColor = (
+	code: number,
+	reset = 0,
+): Color => Object.assign(function colorizer(...args: unknown[]) {
+	const open = `${ESCAPE}${code}m`;
+	const close = `${ESCAPE}${reset}m`;
+	const string = args
+		.map(stringify)
+		.join(' ')
+		.replaceAll(close, `${close}${open}`);
+	return `${open}${string}${close}`;
+}, { toString: ( ) => `${ESCAPE + code}m` });
+
+export const bold = createColor(CODE_BOLD, CODE_RESET_INTENSITY);
+export const dim = createColor(CODE_BOLD, CODE_RESET_INTENSITY);
+export const italic = createColor(CODE_ITALIC, CODE_RESET_ITALIC);
+export const underline = createColor(CODE_UNDERLINE, CODE_RESET_UNDERLINE);
+
+export const black = createColor(CODE_BLACK, CODE_RESET_COLOR);
+export const red = createColor(CODE_RED, CODE_RESET_COLOR);
+export const green = createColor(CODE_GREEN, CODE_RESET_COLOR);
+export const yellow = createColor(CODE_YELLOW, CODE_RESET_COLOR);
+export const blue = createColor(CODE_BLUE, CODE_RESET_COLOR);
+export const magenta = createColor(CODE_MAGENTA, CODE_RESET_COLOR);
+export const cyan = createColor(CODE_CYAN, CODE_RESET_COLOR);
+export const white = createColor(CODE_WHITE, CODE_RESET_COLOR);
---
+++ terrier.ts
@@ -0,0 +1,97 @@
+import { Writable } from 'stream';
+import { inspect } from 'util';
+
+import {
+	bold,
+	cyan,
+	dim,
+	magenta,
+	red,
+	yellow,
+} from './lib/colors.js';
+
+export type LoggerMethod = (...args: unknown[]) => void;
+export type LoggerCreator = (...context: string[]) => Logger;
+export interface LoggerMethods {
+	/** Logs the given arguments with error level priority. */
+	error: LoggerMethod;
+	/** Logs the given arguments with warning level priority. */
+	warn: LoggerMethod;
+	/** Logs the given arguments with info level priority. */
+	info: LoggerMethod;
+	/** Logs the given arguments with debug level priority. */
+	debug: LoggerMethod;
+
+	/** Creates a child logging context of this logger. */
+	child: LoggerCreator;
+}
+/** Logs a message with the default priority level. */
+export type Logger = LoggerMethod & LoggerMethods;
+
+/** The different logger levels available. */
+export enum LoggerLevel {
+	Debug,
+	Info,
+	Warn,
+	Error,
+}
+
+/** Default prefixes for different logger levels. */
+export const DEFAULT_PREFIXES = {
+	[ LoggerLevel.Debug ]: cyan('DBG'),
+	[ LoggerLevel.Info ]: magenta('INF'),
+	[ LoggerLevel.Warn ]: bold(yellow('WRN')),
+	[ LoggerLevel.Error ]: bold(red('ERR')),
+};
+
+const defaultTimestamp = ( ) => dim(new Date( ).toISOString( ));
+const defaultStringify = (thing: unknown) => typeof thing === 'string' ? thing : inspect(thing);
+const defaultPrefix = (level: LoggerLevel) => DEFAULT_PREFIXES[ level ];
+const defaultStream = (level: LoggerLevel) => level > LoggerLevel.Warn
+	? process.stderr
+	: process.stdout;
+
+/** Creates a new logger with the given configuration. */
+export function createLogger({
+	level = LoggerLevel.Debug,
+	timestamp = defaultTimestamp,
+	stringify = defaultStringify,
+	prefix = defaultPrefix,
+	stream = defaultStream,
+	separator = ' ',
+}: {
+	/** The logger's configured level, any messages below this priority level will be ignored. */
+	level?: LoggerLevel;
+	/** A function which generates a string timestamp to be prepended to the the logs. */
+	timestamp?: ( ) => string;
+	/** A function which turns a logged argument into its string representation. */
+	stringify?: (thing: unknown) => string;
+	/** A function which gets the string prefix for a given logger level. */
+	prefix?: (level: LoggerLevel) => string;
+	/** A function which gets the target stream that a logger level should write to. */
+	stream?: (level: LoggerLevel) => Writable;
+	/** The separator to be inserted between context levels. */
+	separator?: string;
+} = { }): Logger {
+	return (function createChild(...context: string[]) {
+		const createMethodForLevel = (
+			targetLevel: LoggerLevel,
+		): LoggerMethod => function log(...args) {
+			if (targetLevel < level) return; // ignored
+			const target = stream(targetLevel);
+			const string = args.map(stringify);
+			const pre = prefix(targetLevel);
+			const ctx = context.join(separator);
+			target.write(`${timestamp( )}${pre}${ctx}${string}\n`);
+		};
+
+		const child = createMethodForLevel(LoggerLevel.Info);
+		return Object.assign(child, {
+			error: createMethodForLevel(LoggerLevel.Error),
+			warn: createMethodForLevel(LoggerLevel.Warn),
+			info: createMethodForLevel(LoggerLevel.Info),
+			debug: createMethodForLevel(LoggerLevel.Debug),
+			child: (...ctx: string[]) => createChild(...context, ...ctx),
+		});
+	})( );
+}
